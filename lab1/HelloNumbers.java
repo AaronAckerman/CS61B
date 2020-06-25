12db6531049c393c75d182569847fef5040938cc
Aaron Qian <Aaron-Mr.Qian@hotmail.com>
2020-06-25 13:24:41 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+//prints out the cumulative sum of the integers from 0 to 9
+public class HelloNumbers{
+	public static void main(String[] args){
+	int x = 0;
+	int n = 0;
+	while (x<=9) {
+	n = n + x;
+	System.out.print(n+ " ");
+	x = x+1;
+	}
+     }
+}
